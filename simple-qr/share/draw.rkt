560c9d0fb4049b831f66260a592c7b724ec2ffc5
Chen Xiao <chenxiao770117@gmail.com>
2019-06-05 11:37:46 +0800

    update

--- simple-qr/share/draw.rkt
+++ simple-qr/share/draw.rkt
@@ -1,13 +1,12 @@
 #lang racket
 
-(require racket/draw)
-
-(require simple-svg)
+(require "draw/png.rkt")
 
 (provide (contract-out
           [locate-brick (-> natural? pair? pair?)]
           [draw-background (-> natural? natural? void?)]
           [draw-points (-> natural? hash? hash? void?)]
+          [draw (-> natural? natural? hash? hash? path-string? void?)]
           [*output_handle* parameter?]
           [*output_type* parameter?]
           ))
@@ -25,7 +24,7 @@
    points_map
    (lambda (point_pair val)
      (let ([new_point_pair (cons (+ (cdr point_pair) 4) (+ (car point_pair) 4))])
-       (draw-module (*output_handle*)
+       (draw-module (*current_handle*)
                     (hash-ref color_map point_pair (if (string=? (~a val) "1") "black" "white"))
                     (locate-brick module_width new_point_pair)
                     module_width)))))
@@ -38,3 +37,24 @@
                   (draw-module (*output_handle*) "white" (locate-brick module_width (cons row col)) module_width)
                   (loop-col (add1 col))))
           (loop-row (add1 row)))))
+
+(define (draw modules module_width points_map color_map file_name)
+  (if
+  (let* ([canvas_width (* (+ modules 8) module_width)]
+         [target #f]
+         [dc #f])
+
+    (set! target (make-bitmap canvas_width canvas_width))
+    (set! dc (new bitmap-dc% [bitmap target]))]
+    
+    (parameterize
+     ([*current_handle* dc])
+
+     (send dc set-smoothing 'aligned)
+
+     (draw-background (+ modules 8) module_width)
+
+     (draw-points module_width points_map color_map)
+
+     (send target save-file file_name 'png)))
+
--- simple-qr/share/draw/png.rkt
+++ simple-qr/share/draw/png.rkt
@@ -2,74 +2,32 @@
 
 (require racket/draw)
 
-(require simple-svg)
-
 (provide (contract-out
-          [locate-brick (-> natural? pair? pair?)]
-          [draw-module (-> (is-a?/c bitmap-dc%) (or/c (is-a?/c color%) string?) pair? natural? void?)]
-          [draw-background (-> (is-a?/c bitmap-dc%) natural? natural? void?)]
-          [draw-points (-> (is-a?/c bitmap-dc%) natural? hash? hash? void?)]
-          [draw (-> natural? natural? hash? hash? path-string? void?)]
-          [*output_type* parameter?]
+          [draw-png-module (-> (or/c (is-a?/c color%) string?) pair? natural? void?)]
+          [draw-png (-> natural? natural? hash? hash? path-string? void?)]
           ))
 
-(define *output_type* (make-parameter 'png))
-
-(define (locate-brick module_width place_pair)
-  (cons (* (sub1 (cdr place_pair)) module_width)
-        (* (sub1 (car place_pair)) module_width)))
-
-(define (draw-points dc module_width points_map color_map)
-  (hash-for-each
-   points_map
-   (lambda (point_pair val)
-     (let ([new_point_pair (cons (+ (cdr point_pair) 4) (+ (car point_pair) 4))])
-       (draw-module dc
-                    (hash-ref color_map point_pair (if (string=? (~a val) "1") "black" "white"))
-                    (locate-brick module_width new_point_pair)
-                    module_width)))))
-
-(define (draw-background dc modules module_width)
-  (let loop-row ([row 1])
-    (when (<= row modules)
-          (let loop-col ([col 1])
-            (when (<= col modules)
-                  (draw-module dc "white" (locate-brick module_width (cons row col)) module_width)
-                  (loop-col (add1 col))))
-          (loop-row (add1 row)))))
-
-(define (draw-module dc color place_pair module_width)
+(define (draw-png-module color place_pair module_width)
   (when (not (string=? color "transparent"))
-        (send dc set-pen color 1 'solid)
-        (send dc set-brush color 'transparent)
+        (send (*current-handle*) set-pen color 1 'solid)
+        (send (*current-handle*) set-brush color 'transparent)
 
-        (send dc draw-rectangle (cdr place_pair) (car place_pair) module_width module_width)))
+        (send (*current-handle*) draw-rectangle (cdr place_pair) (car place_pair) module_width module_width)))
 
-(define (draw modules module_width points_map color_map file_name)
+(define (draw-png modules module_width points_map color_map file_name)
   (let* ([canvas_width (* (+ modules 8) module_width)]
-         [target #f]
-         [dc #f])
+         [target #f])
 
-    (cond
-     [(eq? (*output_type*) 'png)
-      (set! target (make-bitmap canvas_width canvas_width))
-      (set! dc (new bitmap-dc% [bitmap target]))]
-     [(eq? (*output_type*) 'svg)
-      (set! dc (new svg-dc% [width canvas_width] [height canvas_width] [output file_name] [exists 'replace]))
-      (send dc start-doc "start")
-      (send dc start-page)])
+    (set! target (make-bitmap canvas_width canvas_width))
+    (set! dc (new bitmap-dc% [bitmap target]))]
     
-    (send dc set-smoothing 'aligned)
+    (parameterize
+     ([*current_handle* dc])
 
-    (draw-background dc (+ modules 8) module_width)
+     (send dc set-smoothing 'aligned)
 
-    (draw-points dc module_width points_map color_map)
+     (draw-background (+ modules 8) module_width)
 
-    (cond
-     [(eq? (*output_type*) 'png)
-      (send target save-file file_name 'png)]
-     [(eq? (*output_type*) 'svg)
-      (send dc end-page)
-      (send dc end-doc)])
+     (draw-points module_width points_map color_map)
 
-    (void)))
+     (send target save-file file_name 'png)))
